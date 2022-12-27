5e501d7cfb8c544c493a39f483e62f023c0fbedf
Mades Eladrian <mades.eladrian@gmail.com>
2022-12-26 23:10:01 -0300

    ensure AddAccountRepository raises a NotImplementedError if not implemented

--- test/data/contracts/db/account/add_account_repository_test.py
+++ test/data/contracts/db/account/add_account_repository_test.py
@@ -1,4 +1,6 @@
 from inspect import isabstract
+import pytest
+from unittest.mock import patch
 
 from src.data.contracts.db.account import AddAccountRepository
 
@@ -6,3 +8,10 @@ from src.data.contracts.db.account import AddAccountRepository
 class TestAddAccountRepository:
     def test_1_should_AddAccountRepository_is_an_abstract_class(self):
         assert isabstract(AddAccountRepository)
+
+    @patch.multiple(AddAccountRepository, __abstractmethods__=set())
+    def test_2_should_AddAccountRepository_raise_a_NotImplementedError_if_not_implemented(self):
+        add_account_repository = AddAccountRepository()
+
+        with pytest.raises(NotImplementedError, match='Should implement method: add'):
+            add_account_repository.add(data={})
