267683a87edd4f3b76dbbc91322e0e529d0cc10d
Mades Eladrian <mades.eladrian@gmail.com>
2022-12-26 21:36:25 -0300

    ensure SignUpController calls AddAccount with correct values

--- src/presentation/controllers/signup_controller.py
+++ src/presentation/controllers/signup_controller.py
@@ -1,5 +1,8 @@
 from dataclasses import dataclass
 
+from ...domain.features import AddAccount
+from ...domain.params import AddAccountParams
+
 from ..contracts import Validation
 from ..helpers import bad_request, server_error
 from ..params import SignUpControllerRequest
@@ -7,12 +10,18 @@ from ..params import SignUpControllerRequest
 
 @dataclass
 class SignUpController():
+    add_account: AddAccount
     validation: Validation
 
     def handle(self, request: SignUpControllerRequest):
         try:
             if error := self.validation.validate(request):
                 return bad_request(error)
+            self.add_account.add(AddAccountParams(
+                name=request['name'],
+                email=request['email'],
+                password=request['password']
+            ))
 
         except Exception as e:
             return server_error(e)
--- test/presentation/controllers/signup_controller_test.py
+++ test/presentation/controllers/signup_controller_test.py
@@ -12,6 +12,7 @@ from src.presentation.helpers import (
 )
 
 from ...domain.mocks import mock_add_account_params
+from ..mocks.account import AddAccountSpy
 from ..mocks.validation import ValidationSpy
 
 
@@ -22,26 +23,29 @@ class TestSignUpController:
 
     SutTypes = Tuple[
         SignUpController,
+        AddAccountSpy,
         ValidationSpy
     ]
 
     def make_sut(self) -> SutTypes:
+        add_account_spy = AddAccountSpy()
         validation_spy = ValidationSpy()
         sut = SignUpController(
-          validation=validation_spy
+            add_account=add_account_spy,
+            validation=validation_spy
         )
 
-        return sut, validation_spy
+        return sut, add_account_spy, validation_spy
 
     def test_1_should_call_Validation_with_correct_values(self):
-        sut, validation_spy = self.make_sut()
+        sut, _, validation_spy = self.make_sut()
         request = self.params
         sut.handle(request=request)
 
         assert validation_spy.value == request
 
     def test_2_should_return_400_if_Validation_returns_an_error(self):
-        sut, validation_spy = self.make_sut()
+        sut, _, validation_spy = self.make_sut()
         validation_spy.error = MissingParamError(self.faker.word())
         http_response = sut.handle(self.params)
 
@@ -50,10 +54,17 @@ class TestSignUpController:
 
     @patch('test.presentation.mocks.validation.ValidationSpy.validate')
     def test_3_should_return_500_if_Validation_throws(self, mocker):
-        sut, _ = self.make_sut()
+        sut, _, _ = self.make_sut()
         exception = Exception()
         mocker.side_effect = exception
         http_response = sut.handle(request=self.params)
 
         assert http_response['status_code'] == 500
         assert http_response == server_error(error=exception)
+
+    def test_4_should_call_AddAccount_with_correct_values(self):
+        sut, add_account_spy, _ = self.make_sut()
+        request = self.params
+        sut.handle(request=request)
+
+        assert add_account_spy.params == request
