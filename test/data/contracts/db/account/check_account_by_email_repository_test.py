144bce292ccc2b7a111941455210b91688ffce42
Mades Eladrian <mades.eladrian@gmail.com>
2022-12-26 22:32:31 -0300

    ensure CheckAccountByEmailRepository is an abstract class

---
+++ test/data/contracts/db/account/check_account_by_email_repository_test.py
@@ -0,0 +1,8 @@
+from inspect import isabstract
+
+from src.data.contracts.db.account import CheckAccountByEmailRepository
+
+
+class TestCheckAccountByEmailRepository:
+    def test_1_should_CheckAccountByEmailRepository_is_an_abstract_class(self):
+        assert isabstract(CheckAccountByEmailRepository)
