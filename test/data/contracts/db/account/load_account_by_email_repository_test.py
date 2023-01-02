33923e8b54634ce9771b58cf8ea06e639273b850
Mades Eladrian <mades.eladrian@gmail.com>
2023-01-02 20:21:40 -0300

    ensure LoadAccountByEmailRepository raises a NotImplementedError if not implemented

--- test/data/contracts/db/account/load_account_by_email_repository_test.py
+++ test/data/contracts/db/account/load_account_by_email_repository_test.py
@@ -1,4 +1,6 @@
 from inspect import isabstract
+import pytest
+from unittest.mock import patch
 
 from src.data.contracts.db.account import LoadAccountByEmailRepository
 
@@ -6,3 +8,10 @@ from src.data.contracts.db.account import LoadAccountByEmailRepository
 class TestLoadAccountByEmailRepository:
     def test_1_should_LoadAccountByEmailRepository_is_an_abstract_class(self):
         assert isabstract(LoadAccountByEmailRepository)
+
+    @patch.multiple(LoadAccountByEmailRepository, __abstractmethods__=set())
+    def test_2_should_LoadAccountByEmailRepository_raise_a_NotImplementedError_if_not_implemented(self):
+        load_account_by_email_repository = LoadAccountByEmailRepository()
+
+        with pytest.raises(NotImplementedError, match='Should implement method: load_by_email'):
+            load_account_by_email_repository.load_by_email(email='')
