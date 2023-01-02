68fff35475e78d58149ec2f463ba546624e5290c
Mades Eladrian <mades.eladrian@gmail.com>
2023-01-02 20:37:00 -0300

    ensure HashComparer raises a NotImplementedError if not implemented

--- test/data/contracts/cryptography/hash_comparer_test.py
+++ test/data/contracts/cryptography/hash_comparer_test.py
@@ -1,4 +1,6 @@
 from inspect import isabstract
+import pytest
+from unittest.mock import patch
 
 from src.data.contracts.cryptography import HashComparer
 
@@ -6,3 +8,10 @@ from src.data.contracts.cryptography import HashComparer
 class TestHashComparer:
     def test_1_should_HashComparer_is_an_abstract_class(self):
         assert isabstract(HashComparer)
+
+    @patch.multiple(HashComparer, __abstractmethods__=set())
+    def test_2_should_HashComparer_raise_a_NotImplementedError_if_not_implemented(self):
+        hash_comparer = HashComparer()
+
+        with pytest.raises(NotImplementedError, match='Should implement method: verify_password'):
+            hash_comparer.verify_password(plain_password='', hashed_password='')
