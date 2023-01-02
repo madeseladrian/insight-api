ddbbea7f90ef7646f4d140029608610f54175877
Mades Eladrian <mades.eladrian@gmail.com>
2023-01-02 17:59:22 -0300

    LoginController implements Controller

--- src/presentation/controllers/login_controller.py
+++ src/presentation/controllers/login_controller.py
@@ -1,11 +1,11 @@
 from dataclasses import dataclass
-from typing import Any
 
 from ...domain.features import Authentication
 
-from ..contracts import Validation
+from ..contracts import Controller, Validation
 from ..helpers import (
     bad_request,
+    HttpResponse,
     ok,
     server_error,
     unauthorized
@@ -14,11 +14,11 @@ from ..params import LoginControllerRequest
 
 
 @dataclass
-class LoginController:
+class LoginController(Controller):
     authentication: Authentication
     validation: Validation
 
-    def handle(self, request: LoginControllerRequest) -> Any:
+    def handle(self, request: LoginControllerRequest) -> HttpResponse:
         try:
             if error := self.validation.validate(request):
                 return bad_request(error)
