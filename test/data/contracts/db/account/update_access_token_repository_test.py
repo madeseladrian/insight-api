0037a8b1d7677493e01bc2779a2e0b7409d26c5a
Mades Eladrian <mades.eladrian@gmail.com>
2023-01-02 20:40:44 -0300

    ensure UpdateAccessTokenRepository raises a MotImplementedError if not implemented

--- test/data/contracts/db/account/update_access_token_repository_test.py
+++ test/data/contracts/db/account/update_access_token_repository_test.py
@@ -1,4 +1,6 @@
 from inspect import isabstract
+import pytest
+from unittest.mock import patch
 
 from src.data.contracts.db.account import UpdateAccessTokenRepository
 
@@ -6,3 +8,10 @@ from src.data.contracts.db.account import UpdateAccessTokenRepository
 class TestUpdateAccessTokenRepository:
     def test_1_should_UpdateAccessTokenRepository_is_an_abstract_class(self):
         assert isabstract(UpdateAccessTokenRepository)
+
+    @patch.multiple(UpdateAccessTokenRepository, __abstractmethods__=set())
+    def test_2_should_UpdateAccessTokenRepository_raise_a_NotImplementedError_if_not_implemented(self):
+        update_access_token_repository = UpdateAccessTokenRepository()
+
+        with pytest.raises(NotImplementedError, match='Should implement method: update_access_token'):
+            update_access_token_repository.update_access_token(user_id='', token='')
