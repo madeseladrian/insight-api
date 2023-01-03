2caf8aab9b3650a0725713461e274b92c4b93156
Mades Eladrian <mades.eladrian@gmail.com>
2023-01-02 21:10:50 -0300

    ensure DbAuthentication returns data on success

--- src/data/usecases/db_authentication.py
+++ src/data/usecases/db_authentication.py
@@ -1,13 +1,13 @@
 from dataclasses import dataclass
-from typing import Any
+from typing import Optional
 
 from ...domain.features import Authentication
-from ...domain.params import AuthenticationParams
+from ...domain.params import AuthenticationParams, AuthenticationResult
 
 from ..contracts.cryptography import Encrypter, HashComparer
 from ..contracts.db.account import (
-    LoadAccountByEmailRepository,
-    UpdateAccessTokenRepository
+  LoadAccountByEmailRepository,
+  UpdateAccessTokenRepository
 )
 
 
@@ -18,7 +18,7 @@ class DbAuthentication(Authentication):
     loadAccount_by_email_repository: LoadAccountByEmailRepository
     update_access_token_repository: UpdateAccessTokenRepository
 
-    def auth(self, authentication: AuthenticationParams) -> Any:
+    def auth(self, authentication: AuthenticationParams) -> Optional[AuthenticationResult]:
         if account := self.loadAccount_by_email_repository.load_by_email(
             email=authentication['email']
         ):
@@ -31,3 +31,8 @@ class DbAuthentication(Authentication):
                     user_id=account['id'],
                     token=access_token
                 )
+                return AuthenticationResult(
+                    access_token=access_token,
+                    name=account['name']
+                )
+        return None
