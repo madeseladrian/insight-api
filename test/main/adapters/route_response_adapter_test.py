94a057e7130a2fe0db32923726058f36918b323a
Mades Eladrian <mades.eladrian@gmail.com>
2023-01-04 14:10:29 -0300

    ensure RouteResponseAdapter returns correct data on 201

--- test/main/adapters/route_response_adapter_test.py
+++ test/main/adapters/route_response_adapter_test.py
@@ -11,3 +11,11 @@ class TestRouteResponseAdapter:
         )
 
         assert route_response_adapter(http_response) == http_response
+
+    def test_2_should_adapter_return_correct_data_on_201(self):
+        http_response = HttpResponse(
+            status_code=201,
+            body=True
+        )
+
+        assert route_response_adapter(http_response) == http_response
