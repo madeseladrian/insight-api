031493d2eaac3acad0f9aec7fae7394c4aa82a63
Mades Eladrian <mades.eladrian@gmail.com>
2023-01-04 14:12:41 -0300

    ensure RouteResponseAdapter returns a bad request error

--- src/main/adapters/route_response_adapter.py
+++ src/main/adapters/route_response_adapter.py
@@ -11,6 +11,10 @@ def route_response_adapter(http_response: HttpResponse) -> Any:
         case 200: return http_response
         case 201: return http_response
         case 204: return http_response
+        case 400: raise HTTPException(
+          status_code=status.HTTP_400_BAD_REQUEST,
+          detail=f"{http_response['body']}"
+        )
         case _: raise HTTPException(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
             detail=f"{http_response['body']}"
--- test/main/adapters/route_response_adapter_test.py
+++ test/main/adapters/route_response_adapter_test.py
@@ -1,3 +1,6 @@
+from fastapi import HTTPException
+import pytest
+
 from src.presentation.helpers import HttpResponse
 from src.main.adapters import route_response_adapter
 
@@ -27,3 +30,16 @@ class TestRouteResponseAdapter:
         )
 
         assert route_response_adapter(http_response) == http_response
+
+    def test_4_should_adapter_return_a_bad_request_error(self):
+        http_response = HttpResponse(
+            status_code=400,
+            body='Bad Request Error'
+        )
+
+        with pytest.raises(HTTPException) as excinfo:
+            route_response_adapter(http_response)
+
+        assert isinstance(excinfo.value, HTTPException)
+        assert excinfo.value.status_code == http_response['status_code']
+        assert excinfo.value.detail == http_response['body']
