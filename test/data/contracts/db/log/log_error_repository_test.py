638f148f0ad77ab3651ba11897bbfd68175a56d9
Mades Eladrian <mades.eladrian@gmail.com>
2023-01-13 12:37:36 -0300

    ensure LogErrorRepository raises a NotImplementedError if not implemented

--- test/data/contracts/db/log/log_error_repository_test.py
+++ test/data/contracts/db/log/log_error_repository_test.py
@@ -9,9 +9,9 @@ class TestAddAccountRepository:
     def test_1_should_LogErrorRepository_is_an_abstract_class(self):
         assert isabstract(LogErrorRepository)
 
-    @patch.multiple(AddAccountRepository, __abstractmethods__=set())
-    def test_2_should_AddAccountRepository_raise_a_NotImplementedError_if_not_implemented(self):
-        add_account_repository = AddAccountRepository()
+    @patch.multiple(LogErrorRepository, __abstractmethods__=set())
+    def test_2_should_LogErrorRepository_raise_a_NotImplementedError_if_not_implemented(self):
+        log_error_repository = LogErrorRepository()
 
-        with pytest.raises(NotImplementedError, match='Should implement method: add'):
-            add_account_repository.add(data={})
+        with pytest.raises(NotImplementedError, match='Should implement method: log_error'):
+            log_error_repository.log_error(error='')
