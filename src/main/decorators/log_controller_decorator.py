87302b62208a1373115bacbdeeaad472e87efb08
Mades Eladrian <mades.eladrian@gmail.com>
2023-01-13 11:52:02 -0300

    ensure LogControllerDecorator returns the same result of the controller

--- src/main/decorators/log_controller_decorator.py
+++ src/main/decorators/log_controller_decorator.py
@@ -4,12 +4,13 @@ from typing import Any
 from ...data.contracts.db.log import LogErrorRepository
 
 from ...presentation.contracts import Controller
-
+from ...presentation.helpers import HttpResponse
 
 @dataclass
 class LogControllerDecorator(Controller):
     controller: Controller
     log_error_repository: LogErrorRepository
 
-    def handle(self, request: Any) -> Any:
-        self.controller.handle(request)
+    def handle(self, request: Any) -> HttpResponse:
+        http_response = self.controller.handle(request)
+        return http_response
--- test/main/decorators/log_controller_decorator_test.py
+++ test/main/decorators/log_controller_decorator_test.py
@@ -4,7 +4,7 @@ from typing import Tuple
 from src.main.decorators import LogControllerDecorator
 
 from ...data.mocks.log import LogErrorRepositorySpy
-from .mocks import LogControllerSpy
+from .mocks import LogControllerSpy,  mock_server_error
 
 
 class TestLogControllerDecorator:
@@ -32,3 +32,17 @@ class TestLogControllerDecorator:
         sut.handle(self.request)
 
         assert self.request['body'] == log_controller_spy.request['body']
+
+    def test_2_should_return_the_same_result_of_the_controller(self):
+        sut, log_controller_spy, _ = self.make_sut()
+        http_response = sut.handle(self.request)
+
+        assert http_response == log_controller_spy.http_response
+
+    def test_3_should_call_LogErrorRepository_with_correct_error_if_controller_returns_a_server_error(self):
+        sut, log_controller_spy, log_error_repository_spy = self.make_sut()
+        server_error = mock_server_error()
+        log_controller_spy.http_response = server_error
+        sut.handle(self.request)
+
+        assert log_error_repository_spy.error == server_error['body'].error
