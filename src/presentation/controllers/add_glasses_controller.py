fe16da034007f430643645f6d90d60009241853b
Mades Eladrian <mades.eladrian@gmail.com>
2023-01-13 18:01:50 -0300

    ensure AddGlassesController returns 400 if Validation returns an error

--- src/presentation/controllers/add_glasses_controller.py
+++ src/presentation/controllers/add_glasses_controller.py
@@ -1,13 +1,15 @@
 from dataclasses import dataclass
-
+from typing import Any
 # from ...domain.features import AddGlasses
 
 from ..contracts import Validation
+from ..helpers import bad_request
 from ..params import AddGlassesControllerRequest
 
 @dataclass
 class AddGlassesController:
     validation: Validation
 
-    def handle(self, request: AddGlassesControllerRequest) -> None:
-        self.validation.validate(request)
+    def handle(self, request: AddGlassesControllerRequest) -> Any:
+        if error := self.validation.validate(request):
+            return bad_request(error)
