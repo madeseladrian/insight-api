e62f77666254a9589bf06d829e4e77f7338b39bf
Mades Eladrian <mades.eladrian@gmail.com>
2023-01-13 21:38:17 -0300

    ensure AddImageStorage raises a NotImplementedError if not implemented

--- test/data/contracts/db/glasses/add_image_storage_test.py
+++ test/data/contracts/db/glasses/add_image_storage_test.py
@@ -1,4 +1,6 @@
 from inspect import isabstract
+import pytest
+from unittest.mock import patch
 
 from src.data.contracts.db.glasses import AddImageStorage
 
@@ -6,3 +8,10 @@ from src.data.contracts.db.glasses import AddImageStorage
 class TestAddImageStorage:
     def test_1_should_AddImageStorage_is_an_abstract_class(self):
         assert isabstract(AddImageStorage)
+
+    @patch.multiple(AddImageStorage, __abstractmethods__=set())
+    def test_2_should_AddImageStorage_raise_a_NotImplementedError_if_not_implemented(self):
+        add_image_storage = AddImageStorage()
+
+        with pytest.raises(NotImplementedError, match='Should implement method: add_image'):
+            add_image_storage.add_image(image=0)
