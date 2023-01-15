a3a7a7eaa96ef0bc5da9d49caa1ac1f617da128e
Mades Eladrian <mades.eladrian@gmail.com>
2023-01-14 23:20:23 -0300

    ensure AddImageController returns 204 if valid dat is provided

--- src/presentation/controllers/add_image_controller.py
+++ src/presentation/controllers/add_image_controller.py
@@ -1,23 +1,28 @@
 from dataclasses import dataclass
-from typing import Any
 
 from ...domain.features import AddImage
 
-from ..contracts import Validation
-from ..helpers import bad_request, server_error
+from ..contracts import Controller, Validation
+from ..helpers import (
+    bad_request,
+    HttpResponse,
+    no_content,
+    server_error
+)
 from ..params import AddImageControllerRequest
 
 
 @dataclass
-class AddImageController:
+class AddImageController(Controller):
     add_image: AddImage
     validation: Validation
 
-    def handle(self, request: AddImageControllerRequest) -> Any:
+    def handle(self, request: AddImageControllerRequest) -> HttpResponse:
         try:
             if error := self.validation.validate(request):
                 return bad_request(error)
 
             self.add_image.add_image(request)
+            return no_content()
         except Exception as e:
             return server_error(e)
