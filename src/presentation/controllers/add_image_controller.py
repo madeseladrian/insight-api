a1d33808326bb09abd4d7b68f936f1027cac482c
Mades Eladrian <mades.eladrian@gmail.com>
2023-01-14 23:11:01 -0300

    ensure AddImageController returns 500 if Validation throws

--- src/presentation/controllers/add_image_controller.py
+++ src/presentation/controllers/add_image_controller.py
@@ -2,7 +2,7 @@ from dataclasses import dataclass
 from typing import Any
 
 from ..contracts import Validation
-from ..helpers import bad_request
+from ..helpers import bad_request, server_error
 from ..params import AddImageControllerRequest
 
 
@@ -11,5 +11,9 @@ class AddImageController:
     validation: Validation
 
     def handle(self, request: AddImageControllerRequest) -> Any:
-        if error := self.validation.validate(request):
-            return bad_request(error)
+        try:
+            if error := self.validation.validate(request):
+                return bad_request(error)
+
+        except Exception as e:
+            return server_error(e)
