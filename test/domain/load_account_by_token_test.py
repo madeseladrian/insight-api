25e6187b064f350f7936e6ef1bcb6108e009dc8f
Mades Eladrian <mades.eladrian@gmail.com>
2023-01-15 12:13:27 -0300

    ensure LoadAccountByToken raises a NotImplementedError if not implemented

--- test/domain/load_account_by_token_test.py
+++ test/domain/load_account_by_token_test.py
@@ -1,4 +1,6 @@
 from inspect import isabstract
+import pytest
+from unittest.mock import patch
 
 from src.domain.features import LoadAccountByToken
 
@@ -6,3 +8,10 @@ from src.domain.features import LoadAccountByToken
 class TestLoadAccountByToken:
     def test_1_should_LoadAccountByToken_is_an_abstract_class(self):
         assert isabstract(LoadAccountByToken)
+
+    @patch.multiple(LoadAccountByToken, __abstractmethods__=set())
+    def test_2_should_LoadAccountByToken_raise_a_NotImplementedError_if_not_implemented(self):
+        load_account_by_token = LoadAccountByToken()
+
+        with pytest.raises(NotImplementedError, match='Should implement method: load'):
+            load_account_by_token.load(access_token='', role=None)
