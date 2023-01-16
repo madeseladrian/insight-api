e2e6f452275be5e45443d49bf51496fca37cb9bd
Mades Eladrian <mades.eladrian@gmail.com>
2023-01-15 22:56:37 -0300

    ensure GetGlassesRepository raises a NotImplementedError if not implemented

--- test/data/contracts/db/glasses/get_glasses_repository_test.py
+++ test/data/contracts/db/glasses/get_glasses_repository_test.py
@@ -1,4 +1,6 @@
 from inspect import isabstract
+import pytest
+from unittest.mock import patch
 
 from src.data.contracts.db.glasses import GetGlassesRepository
 
@@ -6,3 +8,10 @@ from src.data.contracts.db.glasses import GetGlassesRepository
 class TestGetGlassesRepository:
     def test_1_should_GetGlassesRepository_is_an_abstract_class(self):
         assert isabstract(GetGlassesRepository)
+
+    @patch.multiple(GetGlassesRepository, __abstractmethods__=set())
+    def test_2_should_GetGlassesRepository_raise_a_NotImplementedError_if_not_implemented(self):
+        get_glasses_repository = GetGlassesRepository()
+
+        with pytest.raises(NotImplementedError, match='Should implement method: get'):
+            get_glasses_repository.get(user_id='any_id')
