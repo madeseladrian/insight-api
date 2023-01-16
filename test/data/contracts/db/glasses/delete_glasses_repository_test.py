6ef7da5e7b2c205e5a0257872a94fc0aab795497
Mades Eladrian <mades.eladrian@gmail.com>
2023-01-16 12:05:08 -0300

    ensure DeleteGlassesRepository raises a NotImplementedError if not implemented

--- test/data/contracts/db/glasses/delete_glasses_repository_test.py
+++ test/data/contracts/db/glasses/delete_glasses_repository_test.py
@@ -1,8 +1,19 @@
 from inspect import isabstract
+import pytest
+from unittest.mock import patch
 
 from src.data.contracts.db.glasses import DeleteGlassesRepository
+from src.data.params import DeleteGlassesRepositoryParams
 
 
 class TestDeleteGlassesRepository:
     def test_1_should_DeleteGlassesRepository_is_an_abstract_class(self):
         assert isabstract(DeleteGlassesRepository)
+
+    @patch.multiple(DeleteGlassesRepository, __abstractmethods__=set())
+    def test_2_should_DeleteGlassesRepository_raise_a_NotImplementedError_if_not_implemented(self):
+        params = DeleteGlassesRepositoryParams(glass_id='any_id')
+        delete_glasses_repository = DeleteGlassesRepository()
+
+        with pytest.raises(NotImplementedError, match='Should implement method: delete'):
+            delete_glasses_repository.delete(params)
