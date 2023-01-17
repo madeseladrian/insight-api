1a007d2fc7cbb325c91d54481815646d9bf96689
Mades Eladrian <mades.eladrian@gmail.com>
2023-01-17 20:27:50 -0300

    ensure App create_app is instance of fastapi

---
+++ test/main/config/app_test.py
@@ -0,0 +1,9 @@
+from fastapi import FastAPI
+from src.main.config import create_app
+
+
+class TestApp:
+    app = create_app()
+
+    def test_1_should_create_app_is_instance_of_fastapi(self):
+        assert isinstance(create_app(), FastAPI)
