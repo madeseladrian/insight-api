ba6947ab7831c830f915c72ee739b66ee2676041
Mades Eladrian <mades.eladrian@gmail.com>
2023-01-16 22:39:21 -0300

    ensure DeleteGlassesController returns 204 if valid data is provided

--- src/presentation/controllers/delete_glasses_controller.py
+++ src/presentation/controllers/delete_glasses_controller.py
@@ -1,25 +1,27 @@
 from dataclasses import dataclass
-from typing import Any
 
 from ...domain.features import DeleteGlasses
 
-from ..contracts import Validation
+from ..contracts import Controller, Validation
 from ..helpers import (
     bad_request,
+    HttpResponse,
+    no_content,
     server_error
 )
 from ..params import AddGlassesControllerRequest
 
 
 @dataclass
-class DeleteGlassesController:
+class DeleteGlassesController(Controller):
     delete_glasses: DeleteGlasses
     validation: Validation
 
-    def handle(self, request: AddGlassesControllerRequest) -> Any:
+    def handle(self, request: AddGlassesControllerRequest) -> HttpResponse:
         try:
             if error := self.validation.validate(request):
                 return bad_request(error)
             self.delete_glasses.delete(request)
+            return no_content()
         except Exception as e:
             return server_error(e)
--- test/presentation/controllers/delete_glasses_controller_test.py
+++ test/presentation/controllers/delete_glasses_controller_test.py
@@ -64,3 +64,10 @@ class TestAddGlassesController:
         sut.handle(self.params)
 
         assert delete_glasses_spy.params == self.params
+
+    def test_5_should_return_204_if_valid_data_is_provided(self):
+        sut, _, _ = self.make_sut()
+        http_response = sut.handle(self.params)
+
+        assert http_response['status_code'] == 204
+        assert http_response['body'] is None
