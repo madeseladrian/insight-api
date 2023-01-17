eebdaf3bc3aa99e63ed18a54f2381d7ae528341b
Mades Eladrian <mades.eladrian@gmail.com>
2023-01-17 20:28:24 -0300

    ensure App create_app calls with middlewares

--- test/main/config/app_test.py
+++ test/main/config/app_test.py
@@ -7,3 +7,12 @@ class TestApp:
 
     def test_1_should_create_app_is_instance_of_fastapi(self):
         assert isinstance(create_app(), FastAPI)
+
+    def test_2_should_create_app_calls_with_middlewares(self):
+        middlewares = self.app.user_middleware[0].__dict__
+        middlewares_options = middlewares['options']
+
+        assert middlewares_options['allow_origins'] == ['*']
+        assert middlewares_options['allow_credentials']
+        assert middlewares_options['allow_methods'] == ['*']
+        assert middlewares_options['allow_headers'] == ['*']
