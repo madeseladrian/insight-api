c1e607941fff5cfa842994dfe6503b2fb12aff7d
Mades Eladrian <mades.eladrian@gmail.com>
2023-01-17 11:12:32 -0300

    ensure UpdateGlasses raises a NotImplementedError if not implementede

--- test/domain/update_glasses_test.py
+++ test/domain/update_glasses_test.py
@@ -1,4 +1,6 @@
 from inspect import isabstract
+import pytest
+from unittest.mock import patch
 
 from src.domain.features import UpdateGlasses
 
@@ -6,3 +8,11 @@ from src.domain.features import UpdateGlasses
 class TestUpdateGlasses:
     def test_1_should_UpdateGlasses_is_an_abstract_class(self):
         assert isabstract(UpdateGlasses)
+
+    @patch.multiple(UpdateGlasses, __abstractmethods__=set())
+    def test_2_should_UpdateGlasses_raise_a_NotImplementedError_if_not_implemented(self):
+        params = {}
+        update_glasses = UpdateGlasses()
+
+        with pytest.raises(NotImplementedError, match='Should implement method: update'):
+            update_glasses.update(params)
